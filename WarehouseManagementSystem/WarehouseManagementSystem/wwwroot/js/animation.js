3f43c776869b24495e0c2e63a189018bf6c1e7b3
I549842 <ivelin.vasilev@student.fontys.nl>
2025-04-16 20:13:36 +0200

    Add Landing page animations

---
+++ WarehouseManagementSystem/WarehouseManagementSystem/wwwroot/js/animation.js
@@ -0,0 +1,32 @@
+﻿const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      const el = entry.target;
+      if (entry.isIntersecting) {
+        el.classList.add("opacity-100", "translate-y-0");
+        el.classList.remove("opacity-0", "translate-y-5");
+      } else {
+        el.classList.remove("opacity-100", "translate-y-0");
+        el.classList.add("opacity-0", "translate-y-5");
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  }
+);
+
+document
+  .querySelectorAll(".fade-on-scroll")
+  .forEach((el) => observer.observe(el));
+
+function hoverArrow(isHover) {
+  const img = document.getElementById("arrowIcon");
+  img.src = isHover ? "../images/Arrow 2.svg" : "../images/Arrow 1.svg";
+}
+
+function redirectFunction() {
+  document.getElementById("main").scrollIntoView({
+    behavior: "smooth",
+  });
+}
